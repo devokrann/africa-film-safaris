f2ba1831127d51121abe751c57113210a1cbc548
Kevon Kibochi <91994339+devokrann@users.noreply.github.com>
2024-06-07 13:33:48 +0300

    Add blog card links to image and title

--- components/cards/Blog.tsx
+++ components/cards/Blog.tsx
@@ -3,7 +3,19 @@ import React from "react";
 import NextImage from "next/image";
 import Link from "next/link";
 
-import { AspectRatio, Button, Card, CardSection, Divider, Group, Image, Stack, Text, Title } from "@mantine/core";
+import {
+	Anchor,
+	AspectRatio,
+	Button,
+	Card,
+	CardSection,
+	Divider,
+	Group,
+	Image,
+	Stack,
+	Text,
+	Title,
+} from "@mantine/core";
 
 import link from "@/handlers/parsers/string/link";
 
@@ -19,44 +31,44 @@ export default function Blog({ data }: { data: typeBlog }) {
 		<Card className={classes.card}>
 			<Stack h={"100%"}>
 				<CardSection>
-					<AspectRatio ratio={3 / 2} className={classes.imageContainer}>
-						{data.image ? (
-							<Image
-								src={data.image}
-								alt={data.title}
-								w={"100%"}
-								loading="lazy"
-								component={NextImage}
-								className={classes.image}
-							/>
-						) : (
-							<MediaImage alt={"placeholder"} width={900} height={600} />
-						)}
-					</AspectRatio>
+					<Anchor inherit component={Link} href={`/blog/${link.linkify(data.titleLink)}`}>
+						<AspectRatio ratio={3 / 2} className={classes.imageContainer}>
+							{data.image ? (
+								<Image
+									src={data.image}
+									alt={data.title}
+									w={"100%"}
+									loading="lazy"
+									component={NextImage}
+									className={classes.image}
+								/>
+							) : (
+								<MediaImage alt={"placeholder"} width={900} height={600} />
+							)}
+						</AspectRatio>
+					</Anchor>
 				</CardSection>
 
-				<Title order={3} className={classes.title}>
-					{data.title}
+				<Title order={3} fz={"lg"}>
+					<Anchor
+						inherit
+						component={Link}
+						underline="never"
+						href={`/blog/${link.linkify(data.titleLink)}`}
+						className={classes.link}
+					>
+						{data.title}
+					</Anchor>
 				</Title>
 
 				<Stack justify="space-between" gap={"xl"} h={"100%"}>
 					<Text lineClamp={2}>{data.description}</Text>
-					<Group justify="space-between">
-						<Group gap={"xs"}>
-							<IconCalendar size={16} stroke={1.5} />
-							<Text fw={"bold"} fz={"sm"}>
-								{data.date}
-							</Text>
-						</Group>
 
-						<Button
-							color="secDark.4"
-							variant="light"
-							component={Link}
-							href={`/blog/${link.linkify(data.titleLink)}`}
-						>
-							Read More
-						</Button>
+					<Group gap={"xs"}>
+						<IconCalendar size={16} stroke={1.5} />
+						<Text fw={"bold"} fz={"sm"}>
+							{data.date}
+						</Text>
 					</Group>
 				</Stack>
 			</Stack>
